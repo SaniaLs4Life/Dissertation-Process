207b64b06671b6a5ca78173d836e18f847b8e59c
Hakan Genc <gnchakan1@gmail.com>
2019-01-08 22:14:53 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <h3 class="title">Dissertation Process</h3>
+        <div class="form">
+            <h2>Login Form</h2>
+            <form action="#">
+                <table>
+                    <tr>
+                        <td><input type="text" name="username" placeholder="Username" /></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="password" placeholder="Password" /></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" name="login" value="Login" /></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
